b800bb5473a1b93bcc70821127a4df01d029e4d4
李胜 <1037881273@qq.com>
2018-02-08 10:43:44 +0800

    删除多余代码

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,10 +1,6 @@
-// import Vue from 'vue'
+
 import About from './about.vue'
 
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(About)
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,6 @@
-// import Vue from 'vue'
+
 import Home from './home'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(Home)
